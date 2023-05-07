5d229816c40a556b7d571926ea3fe07cf77ec5ec
Nikitenko Ludmila <117203301+Luda0703@users.noreply.github.com>
2023-05-07 12:40:28 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
 import apiBooks from './js/apiBooks';
 import sectionMainBooks from './js/section-main-books';
+import supportUkraine from './js/support-ukraine';
+// import { iconRender } from './js/iconRender';
