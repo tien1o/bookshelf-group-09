8532779f68246b4a77cb8d0f5f5b3f47ca2366fa
tien1o <118685409+tien1o@users.noreply.github.com>
2023-05-07 12:31:05 +0200

    add section support

--- src/js/iconRender.js
+++ src/js/iconRender.js
@@ -1,59 +1,57 @@
-import {imageOne} from './images/support-ukraine/image-1.png';
-import {imageTwo} from './images/support-ukraine/image-2.png';
-import {imageThree} from './images/support-ukraine/image-3.png';
-import {imageFour} from './images/support-ukraine/image-4.png';
-import {imageFive} from './images/support-ukraine/image-5.png';
-import {imageSix} from './images/support-ukraine/image-6.png';
-import {imageSeven} from './images/support-ukraine/image-7.png';
-import {imageEight} from './images/support-ukraine/image-8.png';
-import {imageNine} from './images/support-ukraine/image-9.png';
-
+import imageOne from '../images/support-ukraine/image-1.png';
+import imageTwo from '../images/support-ukraine/image-2.png';
+import imageThree from '../images/support-ukraine/image-3.png';
+import imageFour from '../images/support-ukraine/image-4.png';
+import imageFive from '../images/support-ukraine/image-5.png';
+import imageSix from '../images/support-ukraine/image-6.png';
+import imageSeven from '../images/support-ukraine/image-7.png';
+import imageEight from '../images/support-ukraine/image-8.png';
+import imageNine from '../images/support-ukraine/image-9.png';
 
 export const iconRender = [
-    {
-      title: '01',
-      url:
-        'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
-      img: imageOne,
-    },
-    {
-      title: '02',
-      url: 'https://www.projecthope.org/country/ukraine/',
-      img: imageTwo,
-    },
-    {
-      title: '03',
-      url: 'https://internationalmedicalcorps.org/country/ukraine/',
-      img: imageFour,
-    },
-    {
-      title: '04',
-      url: 'https://www.msf.org/ukraine',
-      img: imageFive,
-    },
-    {
-      title: '05',
-      url: 'https://www.razomforukraine.org/',
-      img: imageSix,
-    },
-    {
-      title: '06',
-      url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
-      img: imageSeven,
-    },
-    {
-      title: '07',
-      url: 'https://www.wvi.org/emergencies/ukraine',
-      img: imageEight,
-    },
-    {
-      title: '08',
-      url: 'https://prytulafoundation.org/en',
-      img: imageNine,
-    },
-    {
-      title: '09',
-      url: 'https://u24.gov.ua/uk',
-      img: imageThree,
-    },
-  ]
+  {
+    title: '01',
+    url: 'https://www.savethechildren.net/what-we-do/emergencies/ukraine-crisis',
+    img: imageOne,
+  },
+  {
+    title: '02',
+    url: 'https://www.projecthope.org/country/ukraine/',
+    img: imageTwo,
+  },
+  {
+    title: '03',
+    url: 'https://internationalmedicalcorps.org/country/ukraine/',
+    img: imageFour,
+  },
+  {
+    title: '04',
+    url: 'https://www.msf.org/ukraine',
+    img: imageFive,
+  },
+  {
+    title: '05',
+    url: 'https://www.razomforukraine.org/',
+    img: imageSix,
+  },
+  {
+    title: '06',
+    url: 'https://www.actionagainsthunger.org/location/europe/ukraine/',
+    img: imageSeven,
+  },
+  {
+    title: '07',
+    url: 'https://www.wvi.org/emergencies/ukraine',
+    img: imageEight,
+  },
+  {
+    title: '08',
+    url: 'https://prytulafoundation.org/en',
+    img: imageNine,
+  },
+  {
+    title: '09',
+    url: 'https://u24.gov.ua/uk',
+    img: imageThree,
+  },
+];
--- src/js/support-ukraine.js
+++ src/js/support-ukraine.js
@@ -1,9 +1,7 @@
-import from "./sass/_support.scss"
-import { iconRender } from "./iconRender";
+import '../sass/_support.scss';
+import { iconRender } from './iconRender';
 import Swiper from 'swiper';
 import Swiper, { Navigation, Pagination } from 'swiper';
-
-
 
 const listIcon = document.querySelector('.list-icon');
 const icomList = document.querySelector('.icon-list');
@@ -12,44 +10,41 @@ const swiperContainer = document.querySelector('.swiper');
 const itemsMarkup = rendarListIcon(iconRender);
 listIcon.insertAdjacentHTML('beforeend', itemsMarkup);
 
-
-function rendarListIcon (iconRender) {
-      return iconRender.map(({title, url, img }) => {
-         
-     return `
+function rendarListIcon(iconRender) {
+  return iconRender
+    .map(({ title, url, img }) => {
+      return `
         <li class='icon-list swiper-slide'> 
         <p class="title">${title}</p>
         <a class="sl" href="${url}" target="_blank">
         <img class="fl image-slider" src="${img}">
         </a>
         </li>
-        `
-}).join('');
+        `;
+    })
+    .join('');
 }
-   
- 
 
 new Swiper(swiperContainer, {
-   spaceBetween: 20, // расстояние между слайдами
-   direction: 'vertical',
-   modules: [Navigation, Pagination],
-   navigation: {
-      nextEl: '.swiper-button-next',
-    },
+  spaceBetween: 20, // расстояние между слайдами
+  direction: 'vertical',
+  modules: [Navigation, Pagination],
+  navigation: {
+    nextEl: '.swiper-button-next',
+  },
 
-    mousewheel: {
-      sensitivity: 1,
-    },
+  mousewheel: {
+    sensitivity: 1,
+  },
 
-    breakpoints: {
-      320: {
-         slidesPerView: 4, //показывать по 4 превью
-         loop: true,
-       },
-       768: {
-         slidesPerView: 6, //показывать по 6 превью
-         loop: true,
-       }
-    }, 
- 
+  breakpoints: {
+    320: {
+      slidesPerView: 4, //показывать по 4 превью
+      loop: true,
+    },
+    768: {
+      slidesPerView: 6, //показывать по 6 превью
+      loop: true,
+    },
+  },
 });
