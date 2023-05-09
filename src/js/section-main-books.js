c8f0cb66686d53de308dedbdfe67785f44859368
Voloshun555 <voloshunihor@gmail.com>
2023-05-09 22:33:34 +0300

    456

--- src/js/section-main-books.js
+++ src/js/section-main-books.js
@@ -1,5 +1,7 @@
-import { booksApi } from './apiBooks';
+import {booksApi} from './apiBooks'
 import { openModal } from './modal';
+import {image} from '../images/without-Image/noImage-Medium.png'
+
 
 const mainSectionsBooks = document.querySelector('.bookcase');
 let renderWidth = 375;
@@ -7,11 +9,13 @@ let renderWidth = 375;
 window.addEventListener('resize', resizeWindow);
 
 function resizeWindow() {
+ 
+ 
   if (
-    (window.innerWidth > 767 && renderWidth < 768) ||
-    (window.innerWidth > 1439 && renderWidth < 1440) ||
-    (window.innerWidth < 1440 && renderWidth > 1439) ||
-    (window.innerWidth < 768 && renderWidth > 767)
+    window.innerWidth > 767 && renderWidth < 768 ||
+    window.innerWidth > 1439 && renderWidth < 1440 ||
+    window.innerWidth < 1440 && renderWidth > 1439 ||
+    window.innerWidth < 768 && renderWidth > 767
   ) {
     location.reload();
   }
@@ -28,30 +32,37 @@ if (renderWidth < 768) {
 }
 
 export async function renderAllCategories() {
-  const titleAllCategoriesMain = `<h1 class='bookcase__title'>
+  
+   const titleAllCategoriesMain = `<h1 class='bookcase__title'>
           Best Sellers <span class='bookcase__filter'>Books</span>
         </h1>`;
 
-  const containerBooksList = document.createElement('ul');
-  containerBooksList.classList.add('bookshelf__list');
-  containerBooksList.classList.add('list');
-  await booksApi().then(data => {
-    const markap = data
-      .map(({ list_name, books }) => {
-        // console.log(list_name, books)
-        return `<li class='book__card__item'>
+        const containerBooksList = document.createElement('ul');
+        containerBooksList.classList.add('bookshelf__list');
+        containerBooksList.classList.add('list');
+    await booksApi()
+       .then(data => {
+         const markap = data.map(({list_name, books}) =>{
+          // console.log(list_name, books)
+return `<li class='book__card__item'>
 <h2 class='bookshelf__title__category'>${list_name}</h2>
-<ul class='bookshelf__content book-card__list list'>${books
-          .map(
-            ({
-              _id,
-              book_image_height,
-              book_image_width,
-              author,
-              book_image,
-              title,
-            }) => {
-              return `<li class='book-card__item'data-id=${_id}>
+<ul class='bookshelf__content book-card__list list'>${books.map(({
+   _id,
+   book_image_height,
+   book_image_width,
+   author,
+   book_image,
+   title,
+ }) => {
+
+if (!book_image) {
+book_image = image
+book_image_height = 500;
+book_image_width = 330;
+ 
+} 
+
+   return `<li class='book-card__item'data-id=${_id}>
  <a class='book-card__link' href='#' data-id='${_id}'>
    <div class='book-card__wrapper'>
      <img class='book-card__image' src='${book_image}' alt="Here must be book's name"
@@ -68,33 +79,21 @@ export async function renderAllCategories() {
    <p class='book__card__author'>${author ? author : 'Unknown author'}</p>
  </div>
 </li>`;
-            }
-          )
-          .slice(0, renderedBooks)
-          .join('')}
+ }
+)
+.slice(0, renderedBooks)
+.join('')}
 </ul>
 <button class='book__see__more__btn' type='button' data-see-more data-category-name='${list_name}'>see more</button>
-</li>`;
-      })
-      .join('');
-    containerBooksList.innerHTML = markap;
-    mainSectionsBooks.innerHTML = titleAllCategoriesMain;
-    mainSectionsBooks.appendChild(containerBooksList);
+</li>`}).join('')
+containerBooksList.innerHTML = markap;
+mainSectionsBooks.innerHTML = titleAllCategoriesMain;
+mainSectionsBooks.appendChild(containerBooksList)
+         
+ const bookElements = document.querySelectorAll('.book-card__item');
+ bookElements.forEach(el => el.addEventListener('click', (event) => {
+  event.preventDefault()
+  openModal(el)}));
 
-    const bookElements = document.querySelectorAll('.book-card__item');
-    bookElements.forEach(el =>
-      el.addEventListener('click', (event) => {
-        event.preventDefault();
-        openModal(el);
-      })
-    );
-  });
-}
-renderAllCategories();
-
-function handleScrollToElement(element, position = 'start') {
-  element.scrollIntoView({
-    behavior: 'smooth',
-    position,
-  });
-}
+})}
+renderAllCategories()
