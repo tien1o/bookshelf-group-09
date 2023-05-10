711c2c01420b6d54410b218689e457449c106664
tien1o <118685409+tien1o@users.noreply.github.com>
2023-05-10 21:24:18 +0200

    Revert "Merge pull request #30 from tien1o/support-ukraine"
    This reverts commit f18f897235105a59b913e49054f24330e818b5b8, reversing
    changes made to c56253b6ca594fefceb1e18e0e0536b176229e51.

--- src/js/scrollup.js
+++ src/js/scrollup.js
@@ -1,10 +1,14 @@
+const loadMoreBtn = document.querySelector('.scrollup');
 
-let topBtn = document.querySelector(".top-btn");
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 100) {
+    $('.scrollup').fadeIn();
+  } else {
+    $('.scrollup').fadeOut();
+  }
+});
 
-// On Click, Scroll to the page's top, replace 'smooth' with 'auto' if you don't want smooth scrolling
-topBtn.onclick = () => window.scrollTo({ top: 0, behavior: "smooth" });
-
-// On scroll, Show/Hide the btn with animation
-window.onscroll = () => window.scrollY > 500 ? topBtn.style.opacity = 1 : topBtn.style.opacity = 0
-
-
+$('.scrollup').click(function () {
+  $('html, body').animate({ scrollTop: 0 }, 600);
+  return false;
+});
